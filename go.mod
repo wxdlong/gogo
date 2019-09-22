0e4eb881e38d3fb0fd718b7d01eda531168f8682
wxdlong <wxdlong@qq.com>
2019-09-22 23:04:29 +0800

    hello Mod

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/wxdlong/gogo
+
+go 1.13
+
+require github.com/wxdlong/go-mod v0.0.0-20190922145845-c53d121f9dfe
---
+++ helloMod.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/wxdlong/go-mod/hello"
+)
+
+func main() {
+	hello.Mod("github.com/wxdlong/go-mod/hello")
+}
